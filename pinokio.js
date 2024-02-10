eaf46ff9d14bfdb2bf3ae09c30e101ba68078da2
Theun Kohlbeck <t.kohlbeck@gmail.com>
2024-02-11 00:14:29 +0100

    quick change to get it working

--- pinokio.js
+++ pinokio.js
@@ -16,19 +16,25 @@ module.exports = {
         when: "start.json",
         on: "<i class='fa-solid fa-spin fa-circle-notch'></i> Running",
         type: "label",
+        href: "start.json"
       }, {
         when: "start.json",
-        off: "<i class='fa-solid fa-power-off'></i> Start",
+        off: "<i class='fa-solid fa-power-off'></i> Launch",
         href: "start.json?fullscreen=true&run=true",
       }, {
         when: "start.json",
-        on: "<i class='fa-solid fa-rocket'></i> Open InstructIR",
-        href: (session && session.url ? session.url : "http://127.0.0.1:7860"),
+        on: (session && session.url ? "<i class='fa-solid fa-rocket'></i> Open Web UI" : null),
+        href: (session && session.url ? session.url : null),
         target: "_blank"
+      }, {
+        when: "start.json",
+        on: "<i class='fa-solid fa-desktop'></i> Server",
+        href: "start.json?fullscreen=true"
       }]
     } else {
       return [{
         html: '<i class="fa-solid fa-plug"></i> Install',
+        type: "link",
         href: "install.json?run=true&fullscreen=true"
       }]
     }
